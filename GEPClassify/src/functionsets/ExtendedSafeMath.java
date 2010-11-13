c19439ddc7b06505a89916f685c1bc933471060c
Morley Abbott <Morley.Abbott@Gmail.com>
2010-11-13 17:51:58 -0500

    Added more files

---
+++ GEPClassify/src/functionsets/ExtendedSafeMath.java
@@ -0,0 +1,12 @@
+package functionsets;
+
+import functions.*;
+
+public class ExtendedSafeMath extends SafeMath {
+	public void LoadFunctions() {
+		super.LoadFunctions();
+		AddNewFunction(new SafeSqrtFunction());
+		AddNewFunction(new CosFunction());
+		AddNewFunction(new SinFunction());
+	}
+}
